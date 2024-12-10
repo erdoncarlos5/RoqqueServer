78cd48b7057e2a7bdaf1442ecdfeba4843063766
erdoncarlos5 <erdoncarlos5@gmail.com>
2024-12-10 10:01:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "erdoncarlos5@gmail.com";
 const pass = "tkplxxowqvuqtxyg";
-// real 1 jan
+// millah 1 jan
 
 // Middleware
 app.use(express.json());
